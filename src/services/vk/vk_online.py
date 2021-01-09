d26da663979360ca2e6206e72633270b86de840b
Булыгин Евгений Андреевич <ebulygin@ussc.ru>
2021-01-09 20:25:08 +0500

    Добавил алгоритм выведенния диаграммы онлайна TMBT-41

--- src/services/vk/vk_online.py
+++ src/services/vk/vk_online.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from typing import List
+from datetime import datetime, timedelta
+from typing import List, Optional, Tuple
 
 from src.services.user_activity import UserActivity
 
@@ -7,8 +8,84 @@ from src.services.user_activity import UserActivity
 @dataclass
 class VkOnlineNode:
     is_online: bool
-    last_online: str
+    last_online: datetime
+    created_at: datetime
 
 
-def get_vk_online(online_nodes: List[VkOnlineNode]) -> List[UserActivity]:
-    pass
+@dataclass
+class CurrentOnline:
+    is_online: bool
+    start: datetime
+    possible_end: datetime
+
+
+def _remove_seconds_and_microseconds(time: datetime) -> datetime:
+    return time.replace(second=0, microsecond=0)
+
+
+def _make_current(node: VkOnlineNode, delta: timedelta) -> Optional[CurrentOnline]:
+    if not node.is_online:
+        return None
+    return CurrentOnline(
+        is_online=True,
+        start=node.created_at,
+        possible_end=_get_possible_end(node, delta),
+    )
+
+
+def _get_possible_end(node: VkOnlineNode, delta: timedelta) -> datetime:
+    return node.created_at + delta / 2
+
+
+def _merge(
+    current: CurrentOnline, node: VkOnlineNode, delta: timedelta
+) -> Tuple[Optional[CurrentOnline], Optional[UserActivity]]:
+    if not node.is_online:
+        return None, UserActivity('VK', current.start, node.last_online)
+    return (
+        CurrentOnline(
+            is_online=True,
+            start=current.start,
+            possible_end=_get_possible_end(node, delta),
+        ),
+        None,
+    )
+
+
+def get_single_node_activity(
+    node: VkOnlineNode, delta: timedelta
+) -> Optional[UserActivity]:
+    if not node.is_online:
+        return None
+    half_delta = delta / 2
+    created_at = node.created_at
+    return UserActivity(
+        'VK', from_time=created_at - half_delta, to_time=created_at + half_delta
+    )
+
+
+def get_vk_online(
+    online_nodes: List[VkOnlineNode], nodes_time_delta: timedelta
+) -> List[UserActivity]:
+    user_activities: List[UserActivity] = []
+    if len(online_nodes) < 1:
+        return user_activities
+    if len(online_nodes) == 1:
+        activity = get_single_node_activity(online_nodes[0], nodes_time_delta)
+        if activity:
+            user_activities.append(activity)
+        return user_activities
+
+    current: Optional[CurrentOnline] = None
+    for node in online_nodes:
+        current = current or _make_current(node, nodes_time_delta)
+        if not current:
+            continue
+        current, user_activity = _merge(current, node, nodes_time_delta)
+        if user_activity:
+            user_activities.append(user_activity)
+    if current:
+        user_activities.append(
+            UserActivity('VK', from_time=current.start, to_time=current.possible_end)
+        )
+    return user_activities
---
+++ tests/test_vk_online.py
@@ -0,0 +1,135 @@
+from datetime import datetime, timedelta
+
+import pytest
+
+from src.services.user_activity import UserActivity
+from src.services.vk.vk_online import VkOnlineNode, get_vk_online
+
+
+@pytest.fixture()
+def delta():
+    return timedelta(minutes=10)
+
+
+def test_one_active_node_online(datetime_, delta):
+    nodes = [VkOnlineNode(is_online=True, last_online=datetime_, created_at=datetime_)]
+    actual = get_vk_online(online_nodes=nodes, nodes_time_delta=delta)
+    half_delta = delta / 2
+    expected = [
+        UserActivity(
+            name='VK', from_time=datetime_ - half_delta, to_time=datetime_ + half_delta
+        )
+    ]
+    assert actual == expected
+
+
+def test_one_no_active_node(datetime_, delta):
+    nodes = [
+        VkOnlineNode(
+            is_online=False,
+            last_online=datetime_ - timedelta(hours=1),
+            created_at=datetime.now(),
+        )
+    ]
+    actual = get_vk_online(online_nodes=nodes, nodes_time_delta=delta)
+    assert actual == []
+
+
+def test_active_and_then_no_active_node(datetime_, delta):
+    last_online = datetime_ - timedelta(minutes=2)
+    nodes = [
+        VkOnlineNode(
+            is_online=True, last_online=datetime_ - delta, created_at=datetime_ - delta
+        ),
+        VkOnlineNode(is_online=False, last_online=last_online, created_at=datetime_),
+    ]
+    actual = get_vk_online(online_nodes=nodes, nodes_time_delta=delta)
+    expected = [
+        UserActivity(name='VK', from_time=datetime_ - delta, to_time=last_online)
+    ]
+    assert actual == expected
+
+
+def test_two_close_active_nodes(datetime_, delta):
+    nodes = [
+        VkOnlineNode(
+            is_online=True, last_online=datetime_ - delta, created_at=datetime_ - delta
+        ),
+        VkOnlineNode(is_online=True, last_online=datetime_, created_at=datetime_),
+    ]
+    actual = get_vk_online(online_nodes=nodes, nodes_time_delta=delta)
+    expected = [
+        UserActivity(
+            name='VK', from_time=datetime_ - delta, to_time=datetime_ + 0.5 * delta
+        )
+    ]
+    assert actual == expected
+
+
+def test_two_close_offline_nodes(datetime_, delta):
+    last_online = datetime_ - timedelta(hours=2)
+    nodes = [
+        VkOnlineNode(
+            is_online=False, last_online=last_online, created_at=datetime_ - delta
+        ),
+        VkOnlineNode(is_online=False, last_online=last_online, created_at=datetime_),
+    ]
+    actual = get_vk_online(online_nodes=nodes, nodes_time_delta=delta)
+    assert actual == []
+
+
+def test_offline_and_then_online_node(datetime_, delta):
+    nodes = [
+        VkOnlineNode(
+            is_online=False,
+            last_online=datetime_ - timedelta(hours=2),
+            created_at=datetime_ - delta,
+        ),
+        VkOnlineNode(is_online=True, last_online=datetime_, created_at=datetime_),
+    ]
+    actual = get_vk_online(online_nodes=nodes, nodes_time_delta=delta)
+    expected = [
+        UserActivity(name='VK', from_time=datetime_, to_time=datetime_ + 0.5 * delta)
+    ]
+    assert actual == expected
+
+
+def test_online_offline_online_nodes(datetime_, delta):
+    last_online = datetime_ - delta - timedelta(minutes=2)
+    nodes = [
+        VkOnlineNode(
+            is_online=True,
+            last_online=datetime_ - 2 * delta,
+            created_at=datetime_ - 2 * delta,
+        ),
+        VkOnlineNode(
+            is_online=False, last_online=last_online, created_at=datetime_ - delta
+        ),
+        VkOnlineNode(is_online=True, last_online=datetime_, created_at=datetime_),
+    ]
+    actual = get_vk_online(online_nodes=nodes, nodes_time_delta=delta)
+    expected = [
+        UserActivity(name='VK', from_time=datetime_ - 2 * delta, to_time=last_online),
+        UserActivity(name='VK', from_time=datetime_, to_time=datetime_ + 0.5 * delta),
+    ]
+    assert actual == expected
+
+
+def test_offline_online_offline_nodes(datetime_, delta):
+    last_online = datetime_ - timedelta(minutes=2)
+    nodes = [
+        VkOnlineNode(
+            is_online=False,
+            last_online=datetime_ - timedelta(hours=2),
+            created_at=datetime_ - 2 * delta,
+        ),
+        VkOnlineNode(
+            is_online=True, last_online=datetime_ - delta, created_at=datetime_ - delta
+        ),
+        VkOnlineNode(is_online=False, last_online=last_online, created_at=datetime_),
+    ]
+    actual = get_vk_online(online_nodes=nodes, nodes_time_delta=delta)
+    expected = [
+        UserActivity(name='VK', from_time=datetime_ - delta, to_time=last_online),
+    ]
+    assert actual == expected
