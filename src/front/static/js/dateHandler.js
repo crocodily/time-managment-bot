e6cfade26937999ee255caccab12cb17e9862af8
Makar Zachukov <makzarchukov@gmail.com>
2021-01-16 16:24:34 +0500

    Добавил обработку асинхронных запросов на сервер TMBT-65

--- src/front/static/js/dateHandler.js
+++ src/front/static/js/dateHandler.js
@@ -1,12 +1,11 @@
-let formatDate = function(date) {
-    let day = date.getUTCDate().toString().padStart(2, '0');
-    let month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
-    let year = date.getUTCFullYear().toString();
+function renderDate(currentDate) {
+    let formatDate = function(date) {
+        let day = date.getUTCDate().toString().padStart(2, '0');
+        let month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+        let year = date.getUTCFullYear().toString();
 
-    return `${day}.${month}.${year}`;
+        return `${day}.${month}.${year}`;
+    }
+
+    document.querySelector(".date").innerHTML = formatDate(currentDate);
 }
-
-let currentDate = data.date;
-let formattedDate = formatDate(currentDate)
-
-document.querySelector(".date").innerHTML = formattedDate;
